c071da814f0f003dfede5f31e6a57a35a8956e92
Wojciech Jary <wojtekj98@gmail.com>
2024-06-14 15:19:37 +0200

    Hero update

--- src/components/templates/Page.jsx
+++ src/components/templates/Page.jsx
@@ -1,10 +1,11 @@
-import React from "react";
 import Engine from "../organisms/Engine";
 import Hero from "../organisms/Hero";
 
 const Page = () => {
   return (
-    <div>
+    <div
+      className="w-full flex justify-center items-center flex-col 
+    ">
       <Hero />
       <Engine />
     </div>
